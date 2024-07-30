9749719cc27dc8b476f192aefa656e9a15533ba5
Katherine Hamilton <hamilton-katherine@outlook.com>
2024-07-30 08:47:57 +0100

    Fixed circular imports

--- fractalnetworks/__init__.py
+++ fractalnetworks/__init__.py
@@ -2,19 +2,12 @@
 This module contains functions for the analysis of fractal networks. """
 
 # Other module imports
-from .utilities import *
-from .modelgenerator import *
-from .modelanalysis import *
-from .hubrepulsion import *
-from .networkskeleton import *
-from .networkproperties import *
-from .betweennesscentrality import *
-from .maximumexcludedmassburning import *
-from .greedyalgorithm import *
-from .randomsequential import *
-from .determiningfractality import *
-from .boxrenormalisation import *
 from .calculatingreach import *
+from .networkskeleton import *
+from .boxrenormalisation import *
+from .modelanalysis import *
+from .determiningfractality import *
+from .randomsequential import *
 
 
 
